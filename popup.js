586196788a5a25a1736fe90427be889962428400
Sterndata <subscribed-lists@sterndata.com>
2014-11-21 08:02:13 -0600

    fix js default

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-function popUp(URL,pu_Width=1024) {
+function popUp(URL,pu_Width) {
+pu_Width = pu_Width || 1024;
 day = new Date();
 id = day.getTime();
 eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=yes,location=0,statusbar=0,menubar=0,resizable=1,width=' + pu_Width + ',height=768,left = 128,top = 128');");
