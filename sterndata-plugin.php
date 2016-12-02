4ab80ae4b06990458243f3f8b81be52d6e6ed172
Sterndata <subscribed-lists@sterndata.com>
2016-12-01 23:27:57 -0600

    Version bump

--- sterndata-plugin.php
+++ sterndata-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Standard Stuff (SternData)
  * Plugin URI:	https://github.com/sterndata/sdsplugin
  * Description: functions to support shortcodes and popup
- * Version: 2016.09.13
+ * Version: 2016.12.01
  * Author: Stern Data Solutions
  * Author URI: http://www.sterndata.com
  * License: Gnu Public License V2
